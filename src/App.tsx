7c747a51de4630c0f78491fbb754d27e8e6d8645
Caio Barutti <caiohbfurtado@gmail.com>
2020-12-29 14:41:53 -0300

    create app

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text, StatusBar } from 'react-native';
+
+const App: React.FC = () => (
+  <>
+    <StatusBar barStyle="light-content" backgroundColor="#312e38" />
+    <View style={{ flex: 1, backgroundColor: '#312e38' }}>
+      <Text>Hello, World!</Text>
+    </View>
+  </>
+);
+
+export default App;
---
+++ src/pages/SignUp/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import * as S from './styles';
+
+const SignUp: React.FC = () => {
+  return <View />;
+};
+
+export default SignUp;
